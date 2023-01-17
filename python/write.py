513e51036cad4e79c65415f51834275304a7fc89
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-01-17 19:20:15 +0100

    Writing data test

--- python/write.py
+++ python/write.py
@@ -1,20 +1,22 @@
+# https://pypi.org/project/influxdb-client/
+# https://github.com/influxdata/influxdb-client-python/tree/master/examples
+
+import random
 import influxdb_client
+from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# https://www.influxdata.com/blog/getting-started-python-influxdb/
+bucket =    "mybucket"
+org =       "user"
+token =     "8Wp7IUmVGvg_DE_G1sVgiNk-OFqWvdQqsOt9CrNwGB5fM7NNeSYRIeoyMxnXo7Or3Xf0GkVg0ulRrmwj7m5RTA=="
+url =       "http://localhost:8086"
 
-bucket =    "<my-bucket>"
-org =       "<my-org>"
-token =     "<my-token>"
-url=        "192.168.178.158:8086"
+with InfluxDBClient(url=url, token=token, org=org) as client:
+    write_api = client.write_api(write_options=SYNCHRONOUS)
 
-client = influxdb_client.InfluxDBClient(
-    url=url,
-    token=token,
-    org=org
-)
+    for n in range(100):
+        record = influxdb_client.Point("my_measurement").tag("my_location", "Prague").field("my_temperature", random.uniform(-20, 45))
 
-write_api = client.write_api(write_options=SYNCHRONOUS)
-
-p = influxdb_client.Point("my_measurement").tag("location", "Prague").field("temperature", 25.3)
-write_api.write(bucket=bucket, org=org, record=p)
+        write_api.write(
+            bucket=bucket,
+            record=record)
