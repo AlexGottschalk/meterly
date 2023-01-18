03f052184bdaae2c66670bf87303a483052db4f9
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-01-18 10:39:44 +0100

    Testing

--- python/write.py
+++ python/write.py
@@ -6,17 +6,26 @@ import influxdb_client
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-bucket =    "mybucket"
+bucket =    "power_consumption_raw"
 org =       "user"
-token =     "8Wp7IUmVGvg_DE_G1sVgiNk-OFqWvdQqsOt9CrNwGB5fM7NNeSYRIeoyMxnXo7Or3Xf0GkVg0ulRrmwj7m5RTA=="
-url =       "http://localhost:8086"
+token =     "cEo3YEeVEVcBU8xca7rnYD3aG8pp8zzI9xCGxPPXyZiezqGRfNti4oaz5x8rK6Tt9q1nPXgy93X-pIHmAuvezA=="
+url =       "http://influxdb:8086"
+# https://stackoverflow.com/questions/61466799/docker-failed-to-establish-a-new-connection-errno-111-connection-refused
 
-with InfluxDBClient(url=url, token=token, org=org) as client:
-    write_api = client.write_api(write_options=SYNCHRONOUS)
+from threading import Timer
 
-    for n in range(100):
-        record = influxdb_client.Point("my_measurement").tag("my_location", "Prague").field("my_temperature", random.uniform(-20, 45))
+def write_data():
+    print("write_data()")
+
+    with InfluxDBClient(url=url, token=token, org=org) as client:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        record = influxdb_client.Point("electricity_meter").tag("location", "home").tag("sensor_type", "TCRT5000").field("power_per_turn", 75).field("turns", 1)
 
         write_api.write(
             bucket=bucket,
             record=record)
+    
+    Timer(random.randint(30, 120), write_data).start()
+
+write_data()
