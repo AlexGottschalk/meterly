6be55d504f4d982fda161908f78a5b7bd969160e
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-01-25 10:00:59 +0100

    Create influxdb.py

---
+++ python/influxdb.py
@@ -0,0 +1,16 @@
+from influxdb_client import InfluxDBClient
+
+class InfluxDB:
+    def __init__(self, url, token, org, bucket='my_bucket'):
+        self.url = url
+        self.token = token
+        self.org = org
+        self.bucket = bucket
+        self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org)
+        self.write_api = self.client.write_api()
+
+    def write_data(self, data, sync_=True):
+        if sync_:
+            self.write_api.write(data, bucket=self.bucket, write_options=self.write_api.SYNCHRONOUS)
+        else:
+            self.write_api.write(data, bucket=self.bucket)
