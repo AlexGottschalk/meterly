d20714bb6d3bacc5b54e93afc4eca86ac24cb0a4
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 11:17:43 +0100

    +

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,16 +1,21 @@
-from gpiozero import LineSensor
+from time import sleep
 from threading import Thread
+from gpiozero import LineSensor
 
 def event_loop():
     sensor = LineSensor(17)
     sensor.when_line = lambda: print('Line detected')
     sensor.when_no_line = lambda: print('No line detected')
+
+def endless_loop():
     while True:
-        pass
+        sleep(1)
 
 event_thread = Thread(target=event_loop)
 event_thread.start()
 
-# Hier können Sie den Hauptthread fortsetzen und andere Aufgaben ausführen
+endless_thread = Thread(target=endless_loop)
+endless_thread.start()
 
 event_thread.join()
+endless_thread.join()
