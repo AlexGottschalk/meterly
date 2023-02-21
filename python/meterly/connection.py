8496e7d43f13e08fe329a151cf508324391b1458
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 12:33:50 +0100

    Testing

--- python/meterly/connection.py
+++ python/meterly/connection.py
@@ -1,5 +1,5 @@
 import random
-from influxdb_client import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point
 from influxdb_client .client.write_api import SYNCHRONOUS
 
 class InfluxDBConnection:
@@ -12,5 +12,5 @@ class InfluxDBConnection:
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
 
     def write_data(self, data):
-        record = InfluxDBClient.Point("electricity_meter").tag("location", "home").tag("sensor_type", "TCRT5000").field("power_per_turn", 75).field("turns", random.randint(0, 10))
+        record = Point("electricity_meter").tag("location", "home").tag("sensor_type", "TCRT5000").field("power_per_turn", 75).field("turns", random.randint(0, 10))
         self.write_api.write(bucket=self.bucket, record=record)
