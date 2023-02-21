3aec6634dd6ed3d63bb36debe335f2d92a46684a
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 14:07:20 +0100

    Update marking_counter.py

--- python/meterly/marking_counter.py
+++ python/meterly/marking_counter.py
@@ -20,33 +20,33 @@ class MarkingCounter:
     def marking_detected(self):
         self.count += 1
         if callable(self.on_marking_detected):
-            self.on_marking_detected()
+            self.on_marking_detected(self.count)
             
     def marking_passed(self):
         if callable(self.on_marking_passed):
-            self.on_marking_passed()
+            self.on_marking_passed(self.count)
     
     def periodic_job(self):
         if callable(self.on_periodic_job):
-            self.on_periodic_job()
+            self.on_periodic_job(self.count)
                 
     def set_on_marking_detected(self, fn, reset_count = False):
-        def wrapped_fn():
-            fn(self.count)
+        def wrapped_fn(count):
+            fn(count)
             if reset_count:
                 self.count = 0
         self.on_marking_detected = wrapped_fn
         
     def set_on_marking_passed(self, fn, reset_count = False):
-        def wrapped_fn():
-            fn(self.count)
+        def wrapped_fn(count):
+            fn(count)
             if reset_count:
                 self.count = 0
         self.on_marking_passed = wrapped_fn
         
     def set_on_periodic_job(self, fn, reset_count = False):
-        def wrapped_fn():
-            fn(self.count)
+        def wrapped_fn(count):
+            fn(count)
             if reset_count:
                 self.count = 0
         self.on_periodic_job = wrapped_fn
