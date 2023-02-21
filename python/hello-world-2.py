7ac42a8e6ccc8f030d9d1e789b013f80a9d2f12f
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 11:47:53 +0100

    +

--- python/hello-world-2.py
+++ python/hello-world-2.py
@@ -1,5 +1,24 @@
-import time
+import logging
+import multiprocessing
+import sys
 
-while True:
-    print("Hello World!")
-    time.sleep(10)
+import psutil
+from joblib.parallel import Parallel, delayed
+
+def get_logger():
+    logger = logging.getLogger()
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter("[%(process)d/%(processName)s] %(message)s")
+        handler.setFormatter(formatter)
+        handler.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+    return logger
+
+def fn1(n):
+    get_logger().debug("fn1(%d); cpu# %d", n, psutil.Process().cpu_num())
+
+if __name__ == "__main__":
+    get_logger().debug("main")
+    Parallel(n_jobs=multiprocessing.cpu_count())(delayed(fn1)(n) for n in range(1, 101))
