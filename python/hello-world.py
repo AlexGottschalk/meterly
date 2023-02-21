820df1a2fd1d02337bd8a558c09d506d74b07e91
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 10:51:26 +0100

    +

--- python/hello-world.py
+++ python/hello-world.py
@@ -7,15 +7,9 @@ def test():
     sensor = LineSensor(17)
     sensor.when_line = lambda: print('Line detected')
     sensor.when_no_line = lambda: print('No line detected')
-    
-def loop():
-    while True:
-        pass
 
 t1 = multiprocessing.Process(target=test)
-t2 = multiprocessing.Process(target=loop)
 t1.start()
-t2.start()
 
 print("LineSensor")
 #time.sleep(10)
