a98e69e85cb3831d0af08aa8024680e665e227ec
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 10:42:19 +0100

    +

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,18 +1,17 @@
 import time
-import signal
+import multiprocessing
+from signal import pause
 from gpiozero import LineSensor
 
+def test():
+    sensor = LineSensor(17)
+    sensor.when_line = lambda: print('Line detected')
+    sensor.when_no_line = lambda: print('No line detected')
 
-sensor = LineSensor(17)
-sensor.when_line = lambda: print('Line detected')
-sensor.when_no_line = lambda: print('No line detected')
-
-# t1 = multiprocessing.Process(target=test_function)
-# t2 = multiprocessing.Process(target=test_function)
-# t1.start()
-# t2.start()
-
-signal.sigwait()
+t1 = multiprocessing.Process(target=test)
+t2 = multiprocessing.Process(target=pause)
+t1.start()
+t2.start()
 
 print("LineSensor")
 #time.sleep(10)
