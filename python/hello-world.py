90d8cdd1450928b45c8bc6d9f2977cae611ac8a2
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 10:00:44 +0100

    +

--- python/hello-world.py
+++ python/hello-world.py
@@ -2,3 +2,10 @@ print("Hello world!")
 
 import argparse
 from meterly import ConfigReader, InfluxDBConnection, DataPoint, MarkingCounter
+
+def main():
+    print("main")
+
+if __name__ == '__main__':
+    print('__main__')
+    main()
