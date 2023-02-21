862978487ff9e37d6f5f8e3bdfd2ffdad24d39fc
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 11:45:02 +0100

    +

--- python/hello-world-2.py
+++ python/hello-world-2.py
@@ -1,16 +1,5 @@
-import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
-
-try:
-    while True:
-        if GPIO.input(17) == 1:
-            print("I saw the sign!")
-        else: 
-            print("Waiting")
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Bye Bye")
-    GPIO.cleanup()
+while True:
+    print("Hello World!")
+    time.sleep(10)
