d245704ac18f62d45aa48d10c08ce0fa25959e99
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 10:08:56 +0100

    +

--- python/hello-world.py
+++ python/hello-world.py
@@ -1,47 +1,7 @@
-print("Hello world!")
+from gpiozero import LineSensor
+from signal import pause
 
-import argparse
-from meterly import ConfigReader, InfluxDBConnection, DataPoint, MarkingCounter
-
-def main():
-    print("Parse arguments")
-    #region Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', default='python.conf', help='The name of the configuration file.')
-    args = parser.parse_args()
-    #endregion
-    
-    print("Setup the configuration file reader")
-    #region Setup the configuration file reader
-    config = ConfigReader(args.config)
-    #endregion
-    
-    print("Setup the database connection")
-    #region Setup the database connection
-    url = config.get('influxdb', 'url', 'http://localhost:8086')
-    token = config.get('influxdb', 'token', 'my_token')
-    org = config.get('influxdb', 'org', 'my_org')
-    bucket = config.get('influxdb', 'bucket', 'my_bucket')
-    connection = InfluxDBConnection(url, token, org, bucket)
-    #endregion
-    
-    print("Setup the data (to be written to the database)")
-    #region Setup the data (to be written to the database)
-    measurement = config.get('data_point', 'measurement', 'my_measurement')
-    location = config.get('data_point', 'location', 'my_location')
-    sensor_type = config.get('data_point', 'sensor_type', 'my_sensor_type')
-    power_per_turn = config.get('data_point', 'power_per_turn', 75, int)
-    data_point = DataPoint(measurement, location, sensor_type, power_per_turn)
-    #endregion
-    
-    print("Setup the readout of the analog electricity meter")
-    #region Setup the readout of the analog electricity meter
-    pin = config.get('marking_counter', 'pin', 17, int)
-    sample_rate = config.get('marking_counter', 'sample_rate', 1000, int)
-    interval = config.get('marking_counter', 'interval', 60, int)
-    counter = MarkingCounter(pin, sample_rate, interval)
-    #endregion
-
-if __name__ == '__main__':
-    print('__main__')
-    main()
+sensor = LineSensor(17)
+sensor.when_line = lambda: print('Line detected')
+sensor.when_no_line = lambda: print('No line detected')
+pause()
