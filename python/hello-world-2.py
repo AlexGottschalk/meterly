5f8d7bdc79b4ec97c211acb131138ad1a846977b
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-21 11:41:44 +0100

    +

--- python/hello-world-2.py
+++ python/hello-world-2.py
@@ -1,11 +1,16 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
+import time
 
-def button_callback(channel):
-    print("Button was pushed!")
-    
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-GPIO.cleanup() # Clean up
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
+
+try:
+    while True:
+        if GPIO.input(17) is 1:
+            print("I saw the sign!")
+        else: 
+            print("Waiting")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Bye Bye")
+    GPIO.cleanup()
