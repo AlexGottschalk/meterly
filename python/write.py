b67fda2a06e3b50232c8db70f909d9c7e782bea3
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-23 12:29:41 +0100

    Added healtcheck and service_healthy dependecy

--- python/write.py
+++ python/write.py
@@ -4,8 +4,8 @@ from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 bucket =    "power_consumption_raw"
-org =       "user"
-token =     "12y2f61gk08ouac5yp9WE3e_vSlTIGTiKQ3Al2J3B3V5TAFAn1CiKZxMLiXtEGTrAHAirKGlUAY6LTG1kjTCDQ=="
+org =       "data_source"
+token =     "KYw6fFH7RpP1MH6Pq8Dex99AAuuTK8dh5XrqdAE_5Sh80nQBrJb2VIFUlR5nCs1H_34-RW9u7sW_S5QKwQD4RQ=="
 url =       "http://influxdb:8086"
 
 from threading import Timer
