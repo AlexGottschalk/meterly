c9b215f9a9b576a63991d046068d5642ea51c527
AlexGottschalk <gottschalk.alex.96@gmail.com>
2023-02-24 15:44:32 +0100

    Update write.py

--- python/write.py
+++ python/write.py
@@ -1,4 +1,6 @@
+import time
 import random
+import datetime
 import influxdb_client
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -11,17 +13,31 @@ url =       "http://influxdb:8086"
 from threading import Timer
 
 def write_data():
-    print("write_data()")
-
     with InfluxDBClient(url=url, token=token, org=org) as client:
         write_api = client.write_api(write_options=SYNCHRONOUS)
 
-        record = influxdb_client.Point("electricity_meter").tag("location", "home").tag("sensor_type", "TCRT5000").field("power_per_turn", 75).field("turns", random.randint(0, 10))
+        record = influxdb_client.Point("electricity_meter").tag("location", "home").tag("sensor_type", "TCRT5000").field("power_per_turn", 75).field("turns", 1)
 
         write_api.write(
             bucket=bucket,
             record=record)
-    
-    Timer(10, write_data).start()
+        
+        print("{}\t{}".format(datetime.datetime.now(), record))
 
-write_data()
+
+def write_data_cyclic():
+    sleeptime_dlt = 20
+    sleeptime_min = 5
+    sleeptime_max = 90
+    sleeptime = random.randint(sleeptime_min, sleeptime_max)
+    
+    while True:
+        write_data()
+        
+        sleeptime += random.randint(-sleeptime_dlt, sleeptime_dlt)
+        sleeptime = max(sleeptime_min, sleeptime)
+        sleeptime = min(sleeptime_max, sleeptime)
+        
+        time.sleep(sleeptime)
+        
+write_data_cyclic()
